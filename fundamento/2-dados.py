cab79d71e20d650977880d80204f42e7502dc041
Doug Prog <danieldougtattoo@gmail.com>
2025-10-28 14:17:58 -0300

    message

---
+++ fundamento/2-dados.py
@@ -0,0 +1,13 @@
+# PythonFlix
+name = "Haloween"
+yearLaunch = 2023
+noteMovie = 9.9
+planIncluded = False
+
+print(name)
+print(yearLaunch)
+
+print(type(name))
+print(type(yearLaunch))
+print(type(noteMovie))
+print(type(planIncluded))
